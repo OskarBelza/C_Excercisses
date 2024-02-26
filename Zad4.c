ff6f75909e15729e7a01537c5b32df8c0c443fb6
Oskar Bełza <oskar.belza@edu.uekat.pl>
2024-02-26 21:44:09 +0100

    First boundle of excercisse

---
+++ Zad4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main4() {
+    for(int i = 1; i <= 200; i++){
+        if (i % 5 == 0 && i % 7 == 0){
+            printf("FooBar\n");
+        } else if (i % 5 == 0){
+            printf("Foo\n");
+        } else if (i % 7 == 0){
+            printf("Bar\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
