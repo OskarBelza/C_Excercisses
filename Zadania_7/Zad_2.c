3324240e4115f0ca58ddbe8944e11d2b60ee8e1e
Oskar Bełza <oskar.belza@edu.uekat.pl>
2024-05-07 19:56:31 +0200

    test

--- Zadania_7/Zad_2.c
+++ Zadania_7/Zad_2.c
@@ -3,53 +3,57 @@
 #include <string.h>
 #include <ctype.h>
 
-// Funkcja pomocnicza do konwersji słownych reprezentacji liczb na ich numeryczne odpowiedniki
-int wordToNumber(const char *word) {
-    if (strcmp(word, "zero") == 0) return 0;
-    if (strcmp(word, "one") == 0) return 1;
-    if (strcmp(word, "two") == 0) return 2;
-    if (strcmp(word, "three") == 0) return 3;
-    if (strcmp(word, "four") == 0) return 4;
-    if (strcmp(word, "five") == 0) return 5;
-    if (strcmp(word, "six") == 0) return 6;
-    if (strcmp(word, "seven") == 0) return 7;
-    if (strcmp(word, "eight") == 0) return 8;
-    if (strcmp(word, "nine") == 0) return 9;
-    return -1; // Nieznane słowo
-}
+char* getLine(FILE *file) {
+    char *line = NULL; // Pointer to buffer with line from file
+    int ch; // Auxiliary variable with char from line
+    size_t size = 0; // Size of buffer
+    size_t len = 0; // Len of line in time
 
-// Funkcja pomocnicza do konwersji liczby zapisanej jako ciąg znaków na liczbę całkowitą
-int parseNumber(const char *str) {
-    int result = 0;
-    int length = strlen(str);
+    // We iterate through line until we meet new line or end of file
+    while ((ch = fgetc(file)) != EOF && ch != '\n') {
 
-    for (int i = 0; i < length; ++i) {
-        if (isdigit(str[i])) {
-            result = result * 10 + (str[i] - '0');
-        } else if (isalpha(str[i])) {
-            // Jeśli napotkamy słowną reprezentację liczby, użyj funkcji wordToNumber() do konwersji
-            char word[20]; // Zakładamy, że słowo nie będzie dłuższe niż 20 znaków
-            int j = 0;
-            while (isalpha(str[i])) {
-                word[j++] = str[i++];
-            }
-            word[j] = '\0';
-            int num = wordToNumber(word);
-            if (num != -1) {
-                result = result * 10 + num;
-            } else {
-                // Ignorujemy nieznane słowo
-                --i; // cofamy iterator, aby nie pominąć następnego znaku po słowie
+        // We check if buffer has good size if not we double it and reallocate memory
+        if (len + 1 >= size) {
+            size = size == 0 ? 128 : size * 2;
+            line = realloc(line, size);
+            // Condition to check if reallocation was successful
+            if (line == NULL) {
+                perror("Błąd alokacji pamięci");
+                exit(EXIT_FAILURE);
             }
         }
+        // After we increase size of buffer we assign new char to the end of line
+        line[len++] = (char)ch;
     }
 
-    return result;
+    // If End of File we return null
+    if (len == 0 && ch == EOF) {
+        free(line);
+        return NULL;
+    }
+    else{
+        line[len++] = '\0'; // We add null character to the end of line to create string in C
+        return line;
+    }
 }
 
+
 int main() {
-    const char *line = "abcone2threexyz";
-    int number = parseNumber(line);
-    printf("Odczytana liczba: %d\n", number);
+    FILE *file = fopen("C:\\Users\\48512\\CLionProjects\\C_Excercisses\\Zadania_7\\Data_1.txt", "r");
+    if (file == NULL) {
+        perror("File opening error");
+        return EXIT_FAILURE;
+    }
+
+    // We read our file line after line with our function getLine
+    char *line;
+    int sum = 0;
+    while ((line = getLine(file)) != NULL) {
+        sum += numbersExtraction1(line);
+        free(line); // We free memory after each line
+    }
+    printf("%d", sum);
+
+    fclose(file);
     return 0;
 }
